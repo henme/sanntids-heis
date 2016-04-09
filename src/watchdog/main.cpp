9bd9c753e74e6a26306deafde2681e6681bc9fe3
henme <henrikkaald@gmail.com>
2016-04-09 03:27:07 +0200

    Watchdog

--- src/watchdog/main.cpp
+++ src/watchdog/main.cpp
@@ -4,60 +4,50 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/inotify.h>
+#include <signal.h>
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
 
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+#define TIMEOUT     1.5
 
 using namespace std;
 
-int main( int argc, char **argv ) 
-{
-  int i = 0;
+bool changed = false;
+
+void isModified(){
   char buffer[BUF_LEN];
-
-  if(true){
-      string STRING;
-      ifstream infile;
-      infile.open ("backup.txt");
-            while(!infile.eof) // To get you all the lines.
-            {
-              getline(infile,STRING); // Saves the line in STRING.
-              cout<<STRING; // Prints our STRING.
-            }
-      infile.close();
-  }
-
+  changed = false;
   int fd = inotify_init();
 
   if ( fd < 0 ) {
-    perror( "inotify_init" );
+    return;
   }
 
   int wd = inotify_add_watch( fd, "backup.txt", IN_MODIFY);
-  int length = read( fd, buffer, BUF_LEN );
-  
-  cout << "read" << endl;
-
+  //Blocking read
+  int length = read( fd, buffer, BUF_LEN );  
   if ( length < 0 ) {
-    perror( "read" );
+    return;
   }
-
-while(true){
-    while ( i < length ) {
-      struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
-      if ( event->len ) {
-          if ( event->mask & IN_MODIFY ) {
-            cout << "The file " << event->name << "was modified." << endl;
-          }
-      i += EVENT_SIZE + event->len;
-      }
-    }
-    length = read( fd, buffer, BUF_LEN );
-  }
+  changed = true;
   inotify_rm_watch( fd, wd );
 
-  /*closing the INOTIFY instance*/
+  //closing the INOTIFY instance
   close( fd );
+  return;
+}
 
-  exit( 0 );
+int main(){
+  while(true){
+    do{
+      boost::thread t(&isModified);
+      t.timed_join(boost::posix_time::seconds(TIMEOUT));
+    } while(changed == true);
+    //cout << "timed out" << endl;
+    system("kill Heis"); //problems with not superuser?
+    system("./bin/Heis");
+  }
+  exit(0);
 }
